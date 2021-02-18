d6a3d588271edfcb186870ac0ef79bcfcbef413f
ctj01 <flexdancer507@gmail.com>
2021-02-17 19:11:06 -0500

    adding file

--- models/base_model.py
+++ models/base_model.py
@@ -36,11 +36,9 @@ class BaseModel:
         constructor
         """
         if kwargs:
-            [
-                setattr(self, key, cast_datetime(key, value))
-                for key, value in kwargs.items()
-                if key != "__class__"
-            ]
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, cast_datetime(key, value))
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
